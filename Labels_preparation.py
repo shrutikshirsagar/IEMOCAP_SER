1b89381f4d8d41d73e1fd1fa37f94bf3a6b4585f
Shruti Kshirsagar <kshirsagarshruti1@gmail.com>
2021-03-23 19:30:56 -0400

    Add files via upload

--- Labels_preparation.py
+++ Labels_preparation.py
@@ -2,23 +2,28 @@ import os
 import numpy as np
 import pandas as pd
 import csv 
-labels = '/media/amrgaballah/Seagate Backup Plus Drive/IEMOCAP/labels'
-f = open(path, "r")
-out = []
-for line in f:
-    
-   
-    if line.startswith('['):
+labels_input = '/media/amrgaballah/Seagate Backup Plus Drive/IEMOCAP/labels/'
+output = '/media/amrgaballah/Seagate Backup Plus Drive/IEMOCAP/labels_pre/'
+if not os.path.exists(output):
+    os.makedirs(output)
+for path1 in os.listdir(labels_input):
+    path2 = os.path.join(labels_input,path1)
+    f = open(path2, "r")
+    out = []
+    for line in f:
 
-        out.append(line.rstrip())
 
-# print(out)    
-f.close()
+        if line.startswith('['):
 
-out2 = []
-for i in out:
-    a = i.replace("\t", ";")
-    out2.append(a)
-print(out2)    
-df = pd.DataFrame(out2)
-df.to_csv('/home/amrgaballah/Desktop/csvfile11.csv', index=False)
+            out.append(line.rstrip())
+
+    # print(out)    
+    f.close()
+
+    out2 = []
+    for i in out:
+        a = i.replace("\t", ";")
+        out2.append(a)
+    print(out2)    
+    df = pd.DataFrame(out2)
+    df.to_csv(output+path1.split('.')[0]+'.csv', index=False)
